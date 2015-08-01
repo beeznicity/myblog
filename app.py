7771b371ec76e88e4da1afd74b3d8fe0843a1647
Matthew Beasley <mbeasley9@gatech.edu>
2015-08-01 00:04:12 -0400

    added users, login, and logout

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 
 from datetime import date
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+
+from flask.ext.bootstrap import Bootstrap
+
+
+from flask.ext.login import logout_user, login_required, login_user
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.session_protection = 'strong'
+lm.login_view = 'login'
 
 from model import *
+from forms import LoginForm
 
 
 @app.route('/')
@@ -19,7 +30,9 @@ def index():
 		entries.append(e)
 	return render_template('index.html', entries = entries)
 
+
 @app.route('/newpost', methods=['GET', 'POST'])
+@login_required
 def newpost():
 	if request.method=='POST':
 		new_entry = Entry(request.form['title'], request.form['body'], date.today())
@@ -33,4 +46,23 @@ def newpost():
 			return redirect(url_for('index'))
 
 	return render_template('newpost.html', error="")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me.data)
+			return redirect(request.args.get('next') or url_for('index'))
+		flash('Invalid username or password.')
+
+	return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out.')
+	return redirect(url_for('index'))
 
--- model.py
+++ model.py
@@ -1,20 +1,74 @@
-from app import db
-import uuid
+from app import db, lm
+from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Entry(db.Model):
-	__tablename__ = 'entries'
+    __tablename__ = 'entries'
 
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String)
-	body = db.Column(db.Text)
-	date= db.Column(db.Date)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    body = db.Column(db.Text)
+    date= db.Column(db.Date)
 
-	def __init__(self, title, body, date):
+    def __init__(self, title, body, date):
 
-		self.title = title
-		self.body = body
-		self.date=date
+        self.title = title
+        self.body = body
+        self.date=date
 
-	def __repr__(self):
-		return '<id {}>'.format(self.id)
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+
+"""
+    Have begun implementing flask-login to handle users and linking users to posts. Don't really have a clue what i'm doing at this point.
+    Definitely needs deeper dive later to grasp and complete implementation
+
+"""
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index = True, unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(120), index=True, unique=True)
+    #posts = db.relationship("Entry", backref='author', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return str(self.id)
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonyous(self):
+        return False
+
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
